90312a498940fcf8c5920797643778b8ae785346
Jiaqi Ji <iamjijiaqi@gmail.com>
2020-10-28 19:30:31 -0400

    layout

---
+++ demo-eureka-fastpass-service/src/main/java/eureka/demo/DemoEurekaFastpassServiceApplication.java
@@ -0,0 +1,12 @@
+package eureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaFastpassServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaFastpassServiceApplication.class, args);
+	}
+}
